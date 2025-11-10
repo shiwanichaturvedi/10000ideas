9aaf37a2772b1fb9efcc57f8cd7ecfeaa1b255cc
Shiwani Chaturvedi05 <142686907+shiwanichaturvedi@users.noreply.github.com>
2025-11-10 11:39:47 +0530

    Refactor vite.config.js to use ES module syntax

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,26 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const vite_1 = require("vite");
-const plugin_react_1 = __importDefault(require("@vitejs/plugin-react"));
-const url_1 = require("url");
-const path_1 = __importDefault(require("path"));
-const __filename = (0, url_1.fileURLToPath)(import.meta.url);
-const __dirname = path_1.default.dirname(__filename);
-exports.default = (0, vite_1.defineConfig)({
-    plugins: [(0, plugin_react_1.default)()],
-    root: path_1.default.resolve(__dirname, '.'),
-    resolve: {
-        alias: {
-            '@': path_1.default.resolve(__dirname, './src'),
-        },
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [react()],
+  root: path.resolve(__dirname, "."),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    build: {
-        outDir: path_1.default.resolve(__dirname, 'dist'), // Output inside client/
-        emptyOutDir: true
+  },
+  build: {
+    outDir: path.resolve(__dirname, "dist"), // Output inside client/
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:5000",
     },
-    server: {
-        proxy: {
-            '/api': 'http://localhost:5000',
-        },
-    }
+  },
 });
